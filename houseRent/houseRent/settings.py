7063cdeaaa1349f35c53dd9494a8c8b1ef089e0d
Nouri-Muhammad <nouri.muhammad91@gmail.com>
2024-02-08 12:05:08 +0330

    Final Edit

--- houseRent/houseRent/settings.py
+++ houseRent/houseRent/settings.py
@@ -67,7 +67,7 @@ ITEM_PIPELINES = {
    'houseRent.pipelines.MongoDBPipeline': 400,
 }
 MONGO_URI = 'mongodb://localhost:27017/'
-DB_NAME = 'apartments_test'
+DB_NAME = 'apartments'
 COLLECTION_NAME = 'apartment'
 
 # Enable and configure the AutoThrottle extension (disabled by default)
